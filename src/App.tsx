bbc027f411e3dac44feb90c8e16908ac87a7834f
matsubrasil <matsuboy@outlook.com>
2022-02-06 13:10:04 -0300

    feat: add theme: app with custom light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { CssBaseline } from "@mui/material";
-import { AppRoutes } from "routes";
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { AppRoutes } from 'routes';
+import { ThemeProvider } from '@mui/system';
+import { LightTheme } from 'shared/themes';
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={LightTheme}>
       <CssBaseline />
       <AppRoutes />
-    </>
+    </ThemeProvider>
   );
 };
---
+++ src/pages/pagina-inicial.page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material';
+
+export const PaginaInicialPage = () => {
+  return (
+    <Button variant='contained' color='primary'>
+      Teste
+    </Button>
+  );
+};
